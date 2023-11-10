948f9488a347b9ea67680ce56011e623e4046857
Tanzina Naznin Shaila <68160814+tnshaila@users.noreply.github.com>
2023-11-10 07:36:56 +0600

    Update bookings.js

--- bookings.js
+++ bookings.js
@@ -1,27 +1,3 @@
-// const bookingForm = document.getElementById("booking-form");
-// const confirmationPopup = document.getElementById("confirmation-popup");
-// const closePopupButton = document.getElementById("close-popup");
-// const confirmationMessage = document.getElementById("confirmation-message");
-
-// bookingForm.addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     const customerName = document.getElementById("customerName").value;
-//     const destination = document.getElementById("destination").value;
-//     const day = document.getElementById("day").value;
-//     const time = document.getElementById("time").value;
-
-//     const message = `Dear ${customerName}, your booking for ${destination} on ${day} at ${time} is confirmed!`;
-
-//     confirmationMessage.textContent = message;
-//     confirmationPopup.style.display = "block";
-// });
-
-// closePopupButton.addEventListener("click", function () {
-//     confirmationPopup.style.display = "none";
-// });
-
-
 function submitForm() {
     const customerName = document.getElementById("customerName").value;
     const destination = document.getElementById("destination").value;
@@ -33,11 +9,11 @@ function submitForm() {
     confirmationMessage.textContent = message;
     confirmationPopup.style.display = "block";
 
-    document.getElementById("content").innerHTML= `
+    document.getElementById("container").innerHTML= `
       
-      <h4>Hello ${customerName}, you will get scholarship this time</h4>`;
+      <h4>Hello ${customerName}, your booking is confirmed</h4>`;
 
-    return true; // Prevent the form from submitting
+    return true; 
 }
 
 function closePopup() {
